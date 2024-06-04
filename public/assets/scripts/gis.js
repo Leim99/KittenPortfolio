5a6ca6ce27971dd980a26ca3b2868575858d5144
leimin99 <leimimwork@gmail.com>
2024-06-04 15:37:35 +0800

    delete public file

--- public/assets/scripts/gis.js
+++
@@ -1,5 +0,0 @@
-$("body").on("map-ready", function () {
-  map.addEventListener(EMap.Events.onFeatureMouseover, function (e) {
-    parent.$("body").trigger("featureHover", { event: e, map });
-  });
-});
