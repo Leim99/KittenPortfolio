3f35016a9a657bac95481536d9f213e406b8046b
leimin99 <leimimwork@gmail.com>
2024-06-06 18:28:08 +0800

    delete txt

--- src/views/desc/mobilepage.vue
+++ src/views/desc/mobilepage.vue
@@ -2,7 +2,7 @@
  * @Author: Leim99 leiminwork@gmail.com
  * @Date: 2024-04-16 16:46:36
  * @LastEditors: leimin99 leimimwork@gmail.com
- * @LastEditTime: 2024-06-06 18:05:23
+ * @LastEditTime: 2024-06-06 18:22:05
  * @FilePath: /kitten-blog/src/views/desc/mobilepage.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -140,8 +140,8 @@
             position: absolute;
             top: -1px;
             background: #181818;
-            border-top-left-radius: 5px;
-            border-top-right-radius: 5px;
+            border-top-left-radius: 10px;
+            border-top-right-radius: 10px;
         }
         .fade-enter-active, .fade-leave-active {
         transition: opacity 0.5s;
